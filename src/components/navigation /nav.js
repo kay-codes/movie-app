91b650c747c4f481692c82d33c03f38a80bdb620
Michaela Bamfo <mbamfo5@gmail.com>
2021-02-09 16:02:13 -0500

    update

---
+++ src/components/navigation /nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+ class nav extends React.Component{
+render () {
+   return(
+       <ul>
+           <li>Logo</li>
+           <li>Home</li>
+           <li>Movie</li>
+           <li>FAQ</li>
+       </ul>
+   )
+}
+ }
+ export default nav ;
